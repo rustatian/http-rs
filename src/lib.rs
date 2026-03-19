de3d3ff17f3179b67af0448a857d589f3d87b82e
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-03-19 21:28:21 +0100

    chore: update launch.json
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,22 @@ pub fn add(left: u64, right: u64) -> u64 {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use super::*;
 
     #[test]
     fn it_works() {
+            let mut apple_map = HashMap::from([
+        ("Red Delicious", 2),
+        ("Kawana Apple", 5),
+    ]);
         let result = add(2, 2);
+        let s: String = String::from("test");
+        let p = Plugin {
+            name: s,
+            version: String::from("1.0"),
+        };
         assert_eq!(result, 4);
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut apple_map = HashMap::from([("Red Delicious", 2), ("Kawana Apple", 5)]);
+    let s: String = String::from("test");
+    print!("Hello, world! {}", s);
+}
